e637a9d247657caeda33b884e66852025e39fd4b
RubenAHM <hernandezruben2005@gmail.com>
2025-05-20 16:06:36 -0400

    Añadido el primer enemigo con funciones

--- Scripts/hitbox.gd
+++ Scripts/hitbox.gd
@@ -4,3 +4,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.get_name()=="Raider":
 		get_tree().reload_current_scene()
+		#Esta función esta compartida tanto para la hitbos del mapa como para la del area2D del enemigo
+		#Se puede cambiar a futuro
